b0822dc09da1fa773f7bda0f7f1bb776362940cb
KKQQ-SYS <3341025200@qq.com>
2026-02-13 23:29:04 +0800

    s

--- 靶场/ctfchow/pwn82/exp.py
+++ 靶场/ctfchow/pwn82/exp.py
@@ -1,1 +1,9 @@
 from kk import*
+context(arch='i386', os='linux')
+#context(arch='amd64', os='linux')
+#p = remote('192.168.127.12', 10001)
+p = process('./pwn')
+elf = ELF('./pwn')
+libc = ELF('/lib/i386-linux-gnu/libc.so.6')
+#libc = ELF('/lib/x86_64-linux-gnu/libc.so.6')
+
